675c214b8e32cc089d1cef7479d0139b1a6655d2
aqilmustaqim <aqilmustaqim28@gmail.com>
2022-09-02 20:12:03 +0700

    Update Api User By Id

--- app/Controllers/UsersApi.php
+++ app/Controllers/UsersApi.php
@@ -20,4 +20,15 @@ class UsersApi extends ResourceController
 
         return $this->respond($datauser);
     }
+
+    public function show($id = null)
+    {
+        $model = new UsersModel();
+        $data = $model->where('id', $id)->first();
+        if ($data) {
+            return $this->respond($data);
+        } else {
+            return $this->failNotFound('Tidak ditemukan data user');
+        }
+    }
 }
