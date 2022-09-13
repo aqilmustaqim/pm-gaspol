6d15abf9aba3333cd85d9603de11e781fa5b50c8
aqilmustaqim <aqilmustaqim28@gmail.com>
2022-09-13 14:12:40 +0700

    Update Database

--- pmgaspol.sql
+++ pmgaspol.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Sep 05, 2022 at 09:08 AM
+-- Generation Time: Sep 13, 2022 at 09:12 AM
 -- Server version: 10.4.22-MariaDB
 -- PHP Version: 7.4.28
 
@@ -41,7 +41,32 @@ INSERT INTO `detail_project` (`id`, `id_project`, `id_users`) VALUES
 (3, 1, 11),
 (9, 1, 9),
 (11, 2, 9),
-(13, 2, 14);
+(13, 2, 14),
+(14, 10, 9),
+(15, 10, 13),
+(16, 10, 11),
+(17, 1, 13);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `detail_task`
+--
+
+CREATE TABLE `detail_task` (
+  `id` int(11) NOT NULL,
+  `id_task` int(11) NOT NULL,
+  `id_users` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `detail_task`
+--
+
+INSERT INTO `detail_task` (`id`, `id_task`, `id_users`) VALUES
+(1, 4, 9),
+(3, 4, 11),
+(4, 5, 9);
 
 -- --------------------------------------------------------
 
@@ -96,7 +121,9 @@ INSERT INTO `migrations` (`id`, `version`, `class`, `group`, `namespace`, `time`
 (4, '2022-08-25-023821', 'App\\Database\\Migrations\\Team', 'default', 'App', 1661395203, 3),
 (5, '2022-08-26-030703', 'App\\Database\\Migrations\\DetailTeam', 'default', 'App', 1661483432, 4),
 (6, '2022-08-31-073127', 'App\\Database\\Migrations\\Project', 'default', 'App', 1661931332, 5),
-(7, '2022-09-01-074631', 'App\\Database\\Migrations\\DetailProject', 'default', 'App', 1662018644, 6);
+(7, '2022-09-01-074631', 'App\\Database\\Migrations\\DetailProject', 'default', 'App', 1662018644, 6),
+(8, '2022-09-08-054403', 'App\\Database\\Migrations\\Task', 'default', 'App', 1662615984, 7),
+(9, '2022-09-08-090016', 'App\\Database\\Migrations\\DetailTask', 'default', 'App', 1662627738, 8);
 
 -- --------------------------------------------------------
 
@@ -139,7 +166,34 @@ CREATE TABLE `project` (
 
 INSERT INTO `project` (`id`, `id_team`, `nama_project`, `deskripsi_project`, `tanggal_mulai`, `batas_waktu`, `status_project`) VALUES
 (1, 1, 'Gaspol Project', 'Versi Mobile Dan Website', '2022-09-01', '2022-09-30', 1),
-(2, 2, 'APP Sekolah', 'Website Sekolah Dengan Wordpress', '2022-09-01', '2022-09-17', 1);
+(2, 2, 'APP Sekolah', 'Website Sekolah Dengan Wordpress', '2022-09-01', '2022-09-17', 1),
+(10, 1, 'Sekolah Project', 'Membuat Website Sekolah Dengan Wordpress', '2022-09-07', '2022-09-30', 1);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `task`
+--
+
+CREATE TABLE `task` (
+  `id` int(11) NOT NULL,
+  `id_project` int(11) NOT NULL,
+  `nama_task` varchar(225) NOT NULL,
+  `deskripsi_task` text NOT NULL,
+  `tanggal_task` date NOT NULL,
+  `batas_task` date NOT NULL,
+  `status_task` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `task`
+--
+
+INSERT INTO `task` (`id`, `id_project`, `nama_task`, `deskripsi_task`, `tanggal_task`, `batas_task`, `status_task`) VALUES
+(4, 1, 'Fitur Sistem Login', 'Membuat Hak Akses Admin Dan Member', '2022-09-08', '2022-09-10', 0),
+(5, 1, 'Fitur Team', 'Membuat CRUD Dan Pengelompokan Member Ke Team', '2022-09-08', '2022-09-18', 0),
+(6, 1, 'Membuat Fitur Checklist Add Member', 'Membuat Add Member Dengan Centang Checklist Menggunakan Jquery AJAX', '2022-09-08', '2022-09-24', 0),
+(7, 2, 'Fitur Task', 'Membuat Fitur Task Untung Masing Masing Member Sesuai Dengan Team nya', '2022-09-08', '2022-09-12', 0);
 
 -- --------------------------------------------------------
 
@@ -187,14 +241,16 @@ CREATE TABLE `users` (
 --
 
 INSERT INTO `users` (`id`, `nama`, `email`, `password`, `foto`, `role_id`, `posisi_id`, `is_active`, `created_at`, `updated_at`) VALUES
-(8, 'Admin', 'admin@gmail.com', '$2y$10$3dSKgOtt0xcnaOEHbxX0KODUKdTba6AOzBKRuKjQdyLkwi/IYxm6.', '1661049086_e48baacabdb6436d3616.jpg', 1, 1, 1, '2022-08-15 17:02:30', '2022-08-27 15:13:46'),
-(9, 'Aqil', 'aqilmustaqim28@gmail.com', '$2y$10$CcRznk.tEg9IGcsCwDVPUOCWBGP7yh0aWu.QWYmpV2zH42ihmz.6G', '1661839947_392d1e09f434fff697ae.jpg', 2, 2, 1, '2022-08-16 11:34:46', '2022-08-30 13:12:27'),
-(11, 'Nafi', 'sayanafi@gmail.com', '$2y$10$3QBO0ozidYde9E7ufgl/J.2rlNBn7Hm8LPgMWJlDfBbq.BWSF7ne6', 'default.png', 3, 2, 1, '2022-08-23 14:05:36', '2022-08-23 15:07:44'),
-(13, 'Sapril', 'sayasyahfril@gmail.com', '$2y$10$OGHlycxaTLdm6tL74RGl2eL7PnnR0XkY6TM0NaOP7hp5PWlUXOEc2', 'default.png', 3, 1, 1, '2022-08-23 15:12:05', '2022-08-25 14:10:59'),
-(14, 'Udennn', 'uden@gmail.com', '$2y$10$wDQOT9W.5ezCGwZ.AE16UO1RLg2tDrCHvcMerAKP/QkPTchqIR0FO', 'default.png', 3, 1, 1, '2022-08-24 11:02:28', '2022-08-25 14:11:23'),
+(8, 'Admin', 'admin@gmail.com', '0192023a7bbd73250516f069df18b500', '1661049086_e48baacabdb6436d3616.jpg', 1, 1, 1, '2022-08-15 17:02:30', '2022-09-13 14:03:39'),
+(9, 'Aqil', 'aqilmustaqim28@gmail.com', '05e48bd6ea6bbb9cf88ce1dd6ddf809d', '1661839947_392d1e09f434fff697ae.jpg', 2, 2, 1, '2022-08-16 11:34:46', '2022-09-13 14:06:33'),
+(11, 'Nafi', 'sayanafi@gmail.com', 'b2f5091c7857d122e7ac1ff002fd028c', 'default.png', 3, 2, 1, '2022-08-23 14:05:36', '2022-09-13 14:07:06'),
+(13, 'Sapril', 'sayasyahfril@gmail.com', '8f6bb3d2d692ae14f6cb80c69b8ecc01', 'default.png', 3, 1, 1, '2022-08-23 15:12:05', '2022-09-13 14:08:32'),
+(14, 'Udennn', 'uden@gmail.com', 'b63c526ed128e72ef7828ca907485e16', 'default.png', 3, 1, 1, '2022-08-24 11:02:28', '2022-09-13 14:09:19'),
 (15, 'Angga', 'sayaangga@gmail.com', '$2y$10$DcYMEc1BrWpmiGoINO4SsejOOYFd5/eSVRxxWlUxjLLxKL/HUru5i', 'default.png', 3, 1, 0, '2022-08-24 11:05:41', '2022-08-24 11:05:41'),
 (17, 'asd', 'ads@gmail.com', '$2y$10$.ch4pTyd5YQiY/FIKYGIeOOCaho8IO6ZJMFwh.84Vt2kWik30xJGW', 'default.png', 3, 1, 0, '2022-08-24 11:52:22', '2022-08-24 11:52:22'),
-(18, 'asdas', 'asd@gmail.com', '$2y$10$czEogViQXT5Uwkx8dHoqxubgSu09BtgHRl2C5lxioBljqXzwoH.JW', 'default.png', 3, 1, 0, '2022-08-24 11:53:06', '2022-08-24 11:53:06');
+(18, 'asdas', 'asd@gmail.com', '$2y$10$czEogViQXT5Uwkx8dHoqxubgSu09BtgHRl2C5lxioBljqXzwoH.JW', 'default.png', 3, 1, 0, '2022-08-24 11:53:06', '2022-08-24 11:53:06'),
+(20, 'TesApi', 'tesapi@gmail.com', '$2y$10$cKOrYfRP8Hw6b2gY.54jleOvhEqptpxlsI82Td5m1MT4Z5k1cSJG6', 'default.png', 1, 1, 1, '2022-09-06 10:09:55', '2022-09-06 10:09:55'),
+(23, 'Anang', 'anang@gmail.com', '7fea7781ab30e76ab041c7121eaa5cec', 'default.png', 3, 1, 1, '2022-09-13 13:47:58', '2022-09-13 13:47:58');
 
 -- --------------------------------------------------------
 
@@ -227,6 +283,12 @@ ALTER TABLE `detail_project`
   ADD PRIMARY KEY (`id`);
 
 --
+-- Indexes for table `detail_task`
+--
+ALTER TABLE `detail_task`
+  ADD PRIMARY KEY (`id`);
+
+--
 -- Indexes for table `detail_team`
 --
 ALTER TABLE `detail_team`
@@ -248,6 +310,12 @@ ALTER TABLE `position`
 -- Indexes for table `project`
 --
 ALTER TABLE `project`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `task`
+--
+ALTER TABLE `task`
   ADD PRIMARY KEY (`id`);
 
 --
@@ -277,7 +345,13 @@ ALTER TABLE `user_role`
 -- AUTO_INCREMENT for table `detail_project`
 --
 ALTER TABLE `detail_project`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=18;
+
+--
+-- AUTO_INCREMENT for table `detail_task`
+--
+ALTER TABLE `detail_task`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
 --
 -- AUTO_INCREMENT for table `detail_team`
@@ -289,7 +363,7 @@ ALTER TABLE `detail_team`
 -- AUTO_INCREMENT for table `migrations`
 --
 ALTER TABLE `migrations`
-  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
 
 --
 -- AUTO_INCREMENT for table `position`
@@ -301,19 +375,25 @@ ALTER TABLE `position`
 -- AUTO_INCREMENT for table `project`
 --
 ALTER TABLE `project`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
+
+--
+-- AUTO_INCREMENT for table `task`
+--
+ALTER TABLE `task`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
 
 --
 -- AUTO_INCREMENT for table `team`
 --
 ALTER TABLE `team`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
 
 --
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=20;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=24;
 
 --
 -- AUTO_INCREMENT for table `user_role`
