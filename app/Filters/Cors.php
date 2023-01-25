e2ffe79a271b09bca42262370dc581bac63d779c
aqilmustaqim <aqilmustaqim28@gmail.com>
2023-01-25 18:05:13 +0700

    Update Login

--- app/Filters/Cors.php
+++ app/Filters/Cors.php
@@ -12,7 +12,7 @@ class Cors implements FilterInterface
     {
         header('Access-Control-Allow-Origin: *');
         header("Access-Control-Allow-Headers: X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Request-Method, Authorization");
-        header("Access-Control-Allow-Methods: GET, POST, OPTIONS, PUT, DELETE");
+        header("Access-Control-Allow-Methods: GET, POST, OPTIONS, PUT, PATCH, DELETE");
         $method = $_SERVER['REQUEST_METHOD'];
         if ($method == "OPTIONS") {
             die();
